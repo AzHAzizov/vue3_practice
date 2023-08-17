b0ee178167a19515a016566f91a6b5d519a759d3
Azizov Aziz <azjhazizov@gmail.com>
2023-08-17 05:40:20 +0500

    Simple addLike and addDislike is working in new created component App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <div>
+
+        <div>
+            <button @click="addLike">
+                Like
+            </button>
+
+
+            <div>
+                Likes count : {{ like }}
+            </div>
+
+        </div>
+
+        <div>
+            <button @click="addDislike">
+                Dislike
+            </button>
+
+            <div>
+                Displikes count: {{ dislike }}
+            </div>
+        </div>
+
+
+
+    </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+    data() {
+        return {
+            like: 0,
+            dislike: 0,
+        }
+    },
 
-nav {
-  padding: 30px;
+    methods: {
+        addLike() {
+            this.like += 1;
+        },
+        addDislike() {
+            this.dislike += 1;
+        }
+    },
 }
+</script>
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style lang="css" scoped></style>    
