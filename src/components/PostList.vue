b18fe0cc7ba32feb53570e141cb2fcba24eb4186
Azizov Aziz <azjhazizov@gmail.com>
2023-08-18 19:38:34 +0500

    Add animation to Sort Remove and Add posts

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,6 +1,8 @@
 <template>
     <div v-if="posts.length > 0">
-        <post-item @remove="$emit('remove', post)" v-for="post in posts" :post="post" v-bind:key="post.id" />
+        <transition-group name="post-list">
+            <post-item @remove="$emit('remove', post)" v-for="post in posts" :post="post" v-bind:key="post.id" />
+        </transition-group>
     </div>
     <h2 v-else>
         Posts list is empty
@@ -11,22 +13,38 @@
 import PostItem from './PostItem.vue'
 
 
-    export default {
+export default {
 
-        components: {
-            PostItem
-        },
-        props: {
-            posts: {
-                type: Array,
-                required: true
-            }
+    components: {
+        PostItem
+    },
+    props: {
+        posts: {
+            type: Array,
+            required: true
         }
     }
+}
 </script>
 
 <style scoped>
+.post-list-item {
+    display: inline-block;
+    margin-right: 10px;
+}
 
+.post-list-enter-active,
+.post-list-leave-active {
+    transition: all 0.4s ease;
+}
 
+.post-list-enter-from,
+.post-list-leave-to {
+    opacity: 0;
+    transform: translateX(130px);
+}
 
+.post-list-move {
+    transition: transform 0.8s ease;
+  }
 </style>
